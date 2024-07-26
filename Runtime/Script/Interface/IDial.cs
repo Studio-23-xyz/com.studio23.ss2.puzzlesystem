e6d872dd6fd61958c541b207b73a03f972a0412b
B-DeshiDev <ffalsoy@gmail.com>
2024-07-26 10:53:35 +0600

    Update package.json

---
+++ Runtime/Script/Interface/IDial.cs
@@ -0,0 +1,24 @@
+﻿using Studio23.SS2.PuzzleSystem.Data;
+
+namespace Studio23.SS2.PuzzleSystem.Interface
+{
+    /// <summary>
+    /// Interface for a dial that can be rotated, moved, selected, and deselected by the player.
+    /// </summary>
+    public interface IDial
+    {
+        
+        /// <summary>
+        /// Represents information about a dial, including its unique identifier, current value, minimum value, and maximum value.
+        /// </summary>
+        public DialInfo DialInfo { get; set; }
+        
+        /// <summary>
+        /// Updates/Sets the dial to a new value while rotating or moving. Invokes the OnValueChanged event.
+        /// </summary>
+        /// <param name="adjustAmount">New value for the dial.</param>
+        public void AdjustValue(int adjustAmount);
+        public void SetValue(int value);
+
+    }
+}
---
+++ Runtime/Script/Model/PuzzleHints.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Studio23.SS2.PuzzleSystem.Data
+{
+    /// <summary>
+    /// Represents a hint for the puzzle, containing a message and a symbol.
+    /// </summary>
+    public class PuzzleHints
+    {
+        /// <summary>
+        /// Gets or sets the hint message.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets the symbol associated with the hint.
+        /// </summary>
+        public Sprite Symbol { get; set; }
+    }
+}
